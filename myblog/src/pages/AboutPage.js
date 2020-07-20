957434c4b39fb47f7a1e1e616d41105dc37fad15
Austine Gomez <austine.gomez@yahoo.com>
2020-07-20 15:53:49 -0500

    blog files

--- myblog/src/pages/AboutPage.js
+++ myblog/src/pages/AboutPage.js
@@ -4,13 +4,8 @@ const AboutPage = () =>(
 
     <>
         <h1>Hello, welcome to my blog</h1>
-        <p>Welcome to my blog by Austine Gomez, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam commodo libero in leo pulvinar, sed maximus elit finibus.
-        Maecenas posuere risus sed neque dignissim, id interdum est viverra. Sed fringilla sollicitudin magna, sit amet molestie libero semper quis.
-        Maecenas euismod felis sem, in posuere augue facilisis ut. Donec pulvinar diam id facilisis luctus. Duis vel enim et nisl ornare tincidunt.
-        Vivamus aliquet convallis dui et molestie. Integer volutpat ultrices eleifend. Aenean vestibulum, lectus et gravida congue,
-        arcu nibh iaculis ante, ac auctor quam lorem vel nunc. Cras eget elit sapien. Proin blandit metus sit amet enim auctor porttitor.
-        Maecenas eu erat non metus dictum imperdiet at in turpis. Donec lacus ante, pulvinar in iaculis in, tempor id turpis.
-     Nullam a aliquam mauris. Pellentesque semper ut sapien in interdum.</p>
+        <p>Welcome to my blog by Austine Gomez,I am new junior fullstack devloper, with an emphasis more on the front-end. I have a CS 
+        degree and love to write code. I enjoy learning new technologies and the challeneges it brings  </p>
      
     </>
 )
--- myblog/src/pages/NotFoundPage.js
+++ myblog/src/pages/NotFoundPage.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
 const NotFoundPage = ()=>(
+    <div>
     <h1>404: Page Not Found</h1>
+    <span role="img" aria-label="">🙈</span>
+    </div>
 );
 
 export default NotFoundPage;
